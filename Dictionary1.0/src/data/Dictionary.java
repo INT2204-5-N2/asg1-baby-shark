64c6dd269bb9f18473afb0728c596a3febeae709
NguyenTuanVu2105 <37433700+NguyenTuanVu2105@users.noreply.github.com>
2018-10-28 17:30:09 +0700

    Update Dictionary.java

--- Dictionary1.0/src/data/Dictionary.java
+++ Dictionary1.0/src/data/Dictionary.java
@@ -17,13 +17,8 @@ public class Dictionary {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] temp = line.split("\t",2);
-                String detail = "";
-                String[] temp2 = temp[1].split("\t");
-                for (int i=0; i<temp2.length; i++)
-                {
-                    detail = detail.concat(temp2[i]+"\n");
-                }
-                dictionary.put(temp[0],detail);
+                if (temp.length == 2){
+                dictionary.put(temp[0],temp[1]);}
             }
             fileInPutStream.close();
             br.close();
@@ -34,18 +29,63 @@ public class Dictionary {
             e.printStackTrace();
         }
     }
-    public static void addWord(String word,String detail){
+    public static void setDataToFile(TreeMap<String,String> dictionary){
+        try {
+            File file = new File("/Dictionary1.0/src/data/dict.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (String key: dictionary.keySet()) {
+                bw.write(key + "\t" + Dictionary.getDetail(key) + "\n");
+            }
+            fw.close();
+            bw.close();
+        } catch (IOException e){
+
+        } catch (Exception e){
+
+        }
+    }
+    public static void insert1WordToFile(String word, String detail) {
+        try {
+            File file = new File("/Dictionary1.0/src/data/dict.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file, true);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println(word + "\t" + detail);
+            fw.close();
+            pw.close();
+        } catch (IOException e){
+
+        } catch (Exception e){
+
+        }
+    }
+    public static void addWord(String word,String detail) {
         dictionary.put(word,detail);
+        Dictionary.insert1WordToFile(word,detail);
     }
     public static void removeWord(String word){
         dictionary.remove(word);
+        setDataToFile(dictionary);
     }
     public static void editWord(String word, String detail){
         dictionary.remove(word);
         dictionary.put(word,detail);
+        setDataToFile(dictionary);
     }
     public static String getDetail(String word){
-        return dictionary.get(word);
+        String detail = "";
+        String[] temp = dictionary.get(word).split("\t");
+        for (int i=0; i<temp.length; i++)
+        {
+            detail = detail.concat(temp[i]+"\n");
+        }
+        return detail;
     }
     public static ObservableList<String> getWordSearch(String word){
         ObservableList<String> temp = FXCollections.observableArrayList();
